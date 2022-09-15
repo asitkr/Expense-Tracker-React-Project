f44bc21fd19455be4957418fe6f32f3e0b1807b4
asitkr <krsinhasaurabh@gmail.com>
2022-09-15 21:27:43 +0530

    final code

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+function AddTransaction() {
+
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
+    const { addTransaction } = useContext(GlobalContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: Date.now(),
+            text,
+            // amount: +amount
+            amount: parseInt(amount)
+        }
+
+        if(amount) {
+            addTransaction(newTransaction);
+        }
+        setText('');
+        setAmount('');
+    }
+
+  return (
+    <>
+        <h3>Add new transaction</h3>
+        <form onSubmit={handleSubmit}>
+            <div className="form-control">
+                <label htmlFor="text">Text</label>
+                <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text...' />
+            </div>
+            <div className="form-control">
+                <label htmlFor="amount">Amount <br/></label> (negative - expense, positive- expense)
+                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder='Enter amount...' />
+            </div>
+            <button className={!text || !amount ? 'btn-disabled' : 'btn'} type='submit' disabled={!text}>Add transaction</button>
+        </form>
+    </>
+  )
+}
+
+export default AddTransaction;
